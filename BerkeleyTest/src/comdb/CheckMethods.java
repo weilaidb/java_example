bca4405787414fa5256d77e59729ae14e8c7c89b
Your Name <you@example.com>
2017-04-19 22:25:05 +0800

    添加BerkeleyTest for example,ok

---
+++ BerkeleyTest/src/comdb/CheckMethods.java
@@ -0,0 +1,16 @@
+package comdb;
+
+public class CheckMethods {
+
+	public static void PrintDebugMessage(String string) {
+		// TODO Auto-generated method stub
+//		System.out.println(string);
+	}
+
+	public static void PrintInfoMessage(String message) {
+		// TODO Auto-generated method stub
+		System.err.println(message);
+		
+	}
+
+}
