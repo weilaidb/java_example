93387b7fd3308ec4b82da968814063880f504249
Your Name <you@example.com>
2017-04-30 10:43:50 +0800

    删除第一个文档

--- mongotest/src/db/MongoDBJDBC.java
+++ mongotest/src/db/MongoDBJDBC.java
@@ -145,9 +145,52 @@ import com.mongodb.client.MongoDatabase;
 //
 //
 
+//
+////更新文档
+//
+//import org.bson.Document;
+//import com.mongodb.MongoClient;
+//import com.mongodb.client.FindIterable;
+//import com.mongodb.client.MongoCollection;
+//import com.mongodb.client.MongoCursor;
+//import com.mongodb.client.MongoDatabase;
+//import com.mongodb.client.model.Filters;
+//
+//public class MongoDBJDBC{
+//   public static void main( String args[] ){
+//      try{   
+//         // 连接到 mongodb 服务
+//         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+//         
+//         // 连接到数据库
+//         MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");  
+//         System.out.println("Connect to database successfully");
+//         
+//         MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+//         System.out.println("集合 test 选择成功");
+//         
+//         //更新文档   将文档中likes=100的文档修改为likes=200   
+//         collection.updateMany(Filters.eq("likes", 100), new Document("$set",new Document("likes",200)));  
+//         //检索查看结果  
+//         FindIterable<Document> findIterable = collection.find();  
+//         MongoCursor<Document> mongoCursor = findIterable.iterator();  
+//         while(mongoCursor.hasNext()){  
+//            System.out.println(mongoCursor.next());  
+//         }  
+//      
+//      }catch(Exception e){
+//         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+//      }
+//      
+//      
+//   }
+//}
+//
 
-//更新文档
 
+
+
+//删除第一个文档
 import org.bson.Document;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
@@ -161,28 +204,28 @@ public class MongoDBJDBC{
       try{   
          // 连接到 mongodb 服务
          MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-         
+
          // 连接到数据库
          MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");  
          System.out.println("Connect to database successfully");
-         
+
          MongoCollection<Document> collection = mongoDatabase.getCollection("test");
          System.out.println("集合 test 选择成功");
-         
-         //更新文档   将文档中likes=100的文档修改为likes=200   
-         collection.updateMany(Filters.eq("likes", 100), new Document("$set",new Document("likes",200)));  
+
+         //删除符合条件的第一个文档  
+         collection.deleteOne(Filters.eq("likes", 200));  
+         //删除所有符合条件的文档  
+         collection.deleteMany (Filters.eq("likes", 200));  
          //检索查看结果  
          FindIterable<Document> findIterable = collection.find();  
          MongoCursor<Document> mongoCursor = findIterable.iterator();  
          while(mongoCursor.hasNext()){  
-            System.out.println(mongoCursor.next());  
+           System.out.println(mongoCursor.next());  
          }  
-      
+           
       }catch(Exception e){
-         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      }
-      
-      
+        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+     }
    }
 }
 
