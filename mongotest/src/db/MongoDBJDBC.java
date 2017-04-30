3b304631564893e8b51c8506cc473284594becf3
Your Name <you@example.com>
2017-04-30 10:38:59 +0800

    获取集合

--- mongotest/src/db/MongoDBJDBC.java
+++ mongotest/src/db/MongoDBJDBC.java
@@ -3,21 +3,45 @@ package db;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 
+//public class MongoDBJDBC{
+//   public static void main( String args[] ){
+//      try{   
+//      // 连接到 mongodb 服务
+//      MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+//         
+//       
+//      // 连接到数据库
+//      MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");  
+//      System.out.println("Connect to database successfully");
+//      mongoDatabase.createCollection("test");
+//      System.out.println("集合创建成功");
+//        
+//      }catch(Exception e){
+//        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+//     }
+//   }
+//}
+
+import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class MongoDBJDBC{
    public static void main( String args[] ){
       try{   
-      // 连接到 mongodb 服务
-      MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-         
+       // 连接到 mongodb 服务
+         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
        
-      // 连接到数据库
-      MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");  
-      System.out.println("Connect to database successfully");
-      mongoDatabase.createCollection("test");
-      System.out.println("集合创建成功");
-        
+         // 连接到数据库
+         MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");  
+       System.out.println("Connect to database successfully");
+      
+       MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+       System.out.println("集合 test 选择成功");
       }catch(Exception e){
         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
      }
    }
 }
+
